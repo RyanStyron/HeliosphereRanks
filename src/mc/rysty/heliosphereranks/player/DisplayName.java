d4f86846c16ba5ab13d2eb15b0a7b1a923aea0d0
Ryan Styron <ryanstyron1220@gmail.com>
2020-03-23 19:39:49 -0400

    Update DisplayName.java and SetDefaultGroup.java.

--- src/mc/rysty/heliosphereranks/player/DisplayName.java
+++ src/mc/rysty/heliosphereranks/player/DisplayName.java
@@ -9,62 +9,68 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import mc.rysty.heliosphereranks.HelioSphereRanks;
+import mc.rysty.heliosphereranks.utils.GroupsFileManager;
+import mc.rysty.heliosphereranks.utils.PlayersFileManager;
 
 public class DisplayName implements Listener {
 
-	private static HelioSphereRanks plugin = HelioSphereRanks.getInstance();
-	private static FileConfiguration config = plugin.getConfig();
+	private GroupsFileManager groupsFileManager = GroupsFileManager.getInstance();
+	private FileConfiguration groupsFile = groupsFileManager.getData();
+	private PlayersFileManager playersFileManager = PlayersFileManager.getInstance();
+	private FileConfiguration playersFile = playersFileManager.getData();
 
 	@EventHandler
-	public void onChat(AsyncPlayerChatEvent e) {
-		Player p = e.getPlayer();
+	public void onChat(AsyncPlayerChatEvent event) {
+		Player player = event.getPlayer();
 
-		setDisplayName(p);
+		setDisplayName(player);
 	}
 
 	@EventHandler
-	public void onJoin(PlayerJoinEvent e) {
-		Player p = e.getPlayer();
+	public void onJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
 
-		setDisplayName(p);
+		setDisplayName(player);
 	}
 
-	public static void setDisplayName(Player player) {
+	private void setDisplayName(Player player) {
 		UUID playerId = player.getUniqueId();
 		String playerName = player.getName();
 
-		// nickname only
-		if (config.getString("Players." + playerId + ".nickname") != null) {
-			player.setDisplayName(config.getString("Players." + playerId + ".nickname"));
+		// Set the player's display name to only their nickname.
+		if (playersFile.getString("Players." + playerId + ".nickname") != null) {
+			player.setDisplayName(playersFile.getString("Players." + playerId + ".nickname"));
 		}
-		// check for group
-		if (config.getString("Players." + playerId + ".group") != null) {
-			String playerGroup = config.getString("Players." + playerId + ".group");
 
-			// group prefix only
-			if (config.getString("Groups." + playerGroup + ".prefix") != null) {
-				String groupPrefix = config.getString("Groups." + playerGroup + ".prefix").replaceAll("&", "ง") + "";
+		// Check for if the player is a member of a group.
+		if (playersFile.getString("Players." + playerId + ".group") != null) {
+			String playerGroup = playersFile.getString("Players." + playerId + ".group");
+
+			// Set the player's display name to only their group's prefix if it has one.
+			if (groupsFile.getString("Groups." + playerGroup + ".prefix") != null) {
+				String groupPrefix = groupsFile.getString("Groups." + playerGroup + ".prefix").replaceAll("&", "ยง");
 
 				player.setDisplayName(groupPrefix + playerName);
-				// group prefix and nickname
-				if (config.getString("Players." + playerId + ".nickname") != null) {
-					String nickname = config.getString("Players." + playerId + ".nickname");
+
+				// Set the player's display name to their group's prefix and nickname if they.
+				if (playersFile.getString("Players." + playerId + ".nickname") != null) {
+					String nickname = playersFile.getString("Players." + playerId + ".nickname");
 
 					player.setDisplayName(groupPrefix + nickname);
 				}
 			}
-			// personal prefix only
-			if (config.getString("Players." + playerId + ".prefix") != null) {
-				String personalPrefix = config.getString("Players." + playerId + ".prefix").replaceAll("&", "ง") + "";
+		}
 
-				player.setDisplayName(personalPrefix + playerName);
-				// personal prefix and nickname
-				if (config.getString("Players." + playerId + ".nickname") != null) {
-					String nickname = config.getString("Players." + playerId + ".nickname");
+		// Set the player's display name to their personal prefix if they have one.
+		if (playersFile.getString("Players." + playerId + ".prefix") != null) {
+			String personalPrefix = playersFile.getString("Players." + playerId + ".prefix").replaceAll("&", "ยง");
 
-					player.setDisplayName(personalPrefix + nickname);
-				}
+			player.setDisplayName(personalPrefix + playerName);
+			// Set the player's display name to their personal prefix and nickname.
+			if (playersFile.getString("Players." + playerId + ".nickname") != null) {
+				String nickname = playersFile.getString("Players." + playerId + ".nickname");
+
+				player.setDisplayName(personalPrefix + nickname);
 			}
 		}
 	}
--- src/mc/rysty/heliosphereranks/player/SetDefaultGroup.java
+++ src/mc/rysty/heliosphereranks/player/SetDefaultGroup.java
@@ -8,22 +8,21 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import mc.rysty.heliosphereranks.HelioSphereRanks;
+import mc.rysty.heliosphereranks.utils.PlayersFileManager;
 
 public class SetDefaultGroup implements Listener {
 
-	private HelioSphereRanks plugin = HelioSphereRanks.getInstance();
+	private PlayersFileManager playersFileManager = PlayersFileManager.getInstance();
+	private FileConfiguration playersFile = playersFileManager.getData();
 
 	@EventHandler
 	public void onJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 		UUID playerId = player.getUniqueId();
-		FileConfiguration config = plugin.getConfig();
 
-		if (config.getString("Players." + playerId + ".group") == null) {
-			config.createSection("Players." + playerId + ".group");
-			config.set("Players." + playerId + ".group", "user");
-			plugin.saveConfig();
+		if (playersFile.getString("Players." + playerId + ".group") == null) {
+			playersFile.set("Players." + playerId + ".group", "user");
+			playersFileManager.saveData();
 		}
 	}
 }
