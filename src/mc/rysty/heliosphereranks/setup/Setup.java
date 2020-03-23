924fb98ecafe59821e7bace8afe12eabb0d8132a
Ryan Styron <ryanstyron1220@gmail.com>
2020-03-23 19:55:25 -0400

    Update Setup.java.

--- src/mc/rysty/heliosphereranks/setup/Setup.java
+++ src/mc/rysty/heliosphereranks/setup/Setup.java
@@ -13,31 +13,38 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.permissions.PermissionAttachment;
 
 import mc.rysty.heliosphereranks.HelioSphereRanks;
+import mc.rysty.heliosphereranks.utils.GroupsFileManager;
+import mc.rysty.heliosphereranks.utils.PlayersFileManager;
 
 public class Setup implements Listener {
 
 	private static HelioSphereRanks plugin = HelioSphereRanks.getInstance();
-	private static FileConfiguration config = plugin.getConfig();
+	private static FileConfiguration playersFile = PlayersFileManager.getInstance().getData();
+	private static FileConfiguration groupsFile = GroupsFileManager.getInstance().getData();
+
 	private static HashMap<UUID, PermissionAttachment> playerPermissions = HelioSphereRanks.playerPermissions;
 
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
-		
+
 		setupPermissions(player);
 	}
 
 	@EventHandler
-	public void onPlayerLevae(PlayerQuitEvent event) {
+	public void onPlayerLeave(PlayerQuitEvent event) {
 		Player player = event.getPlayer();
 		playerPermissions.remove(player.getUniqueId());
 	}
 
 	public static void setupPermissions(Player player) {
-		PermissionAttachment attachment = player.addAttachment(HelioSphereRanks.getInstance());
-		playerPermissions.put(player.getUniqueId(), attachment);
-		permissionsSetter(player.getUniqueId());
+		if (playerPermissions != null)
+			playerPermissions.clear();
 
+		PermissionAttachment attachment = player.addAttachment(plugin);
+		playerPermissions.put(player.getUniqueId(), attachment);
+
+		permissionsSetter(player.getUniqueId());
 	}
 
 	private static void permissionsSetter(UUID uuid) {
@@ -45,14 +52,14 @@ public class Setup implements Listener {
 		UUID playerId = Bukkit.getPlayer(uuid).getUniqueId();
 		String playerName = Bukkit.getPlayer(uuid).getName();
 
-		if (config.getString("Players." + playerId + ".group") != null) {
-			String playerGroup = config.getString("Players." + playerId + ".group");
+		if (playersFile.getString("Players." + playerId + ".group") != null) {
+			String playerGroup = playersFile.getString("Players." + playerId + ".group");
 
-			if (config.getString("Groups." + playerGroup) != null) {
+			if (groupsFile.getString("Groups." + playerGroup) != null) {
 				System.out.println("HS-Ranks: " + playerName + " has a group that exists.");
 				System.out.println("HS-Ranks: " + playerName + " is a member of group " + playerGroup + ".");
 
-				for (String playerPermissions : config.getStringList("Groups." + playerGroup + ".permissions")) {
+				for (String playerPermissions : groupsFile.getStringList("Groups." + playerGroup + ".permissions")) {
 					if (playerPermissions != null) {
 						System.out.println(playerName + "'s Permissions: " + playerPermissions);
 						attachment.setPermission(playerPermissions, true);
@@ -63,7 +70,6 @@ public class Setup implements Listener {
 			} else {
 				System.out.println("HS-Ranks: " + playerName + "'s group is not valid.");
 			}
-
 		} else {
 			System.out.println("HS-Ranks: " + playerName + " has no group.");
 		}
