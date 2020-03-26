56392f972b8956e26131389c1696c4a4da9f32c1
Ryan Styron <ryanstyron1220@gmail.com>
2020-03-26 13:06:35 -0400

    Add messages in Prefix.java to config.yml.

--- src/mc/rysty/heliosphereranks/commands/Prefix.java
+++ src/mc/rysty/heliosphereranks/commands/Prefix.java
@@ -31,16 +31,14 @@ public class Prefix implements CommandExecutor {
 				String prefix = "";
 
 				if (args.length == 0) {
-					MessageUtils.message(sender,
-							"&4&l(!)&c Not enough arguments were provided! Correct format: /prefix [player] <prefix>");
+					MessageUtils.configStringMessage(sender, "prefix.argument-error");
 					return false;
 				} else if (args.length == 1) {
 					if (sender instanceof Player) {
 						target = (Player) sender;
 						prefix = args[0];
 					} else {
-						MessageUtils.message(sender,
-								"&4&l(!)&c Not enough arguments were provided! Correct format: /prefix [player] <prefix>");
+						MessageUtils.configStringMessage(sender, "prefix.argument-error");
 					}
 				} else if (args.length == 2) {
 					target = Bukkit.getPlayer(args[0]);
@@ -51,8 +49,7 @@ public class Prefix implements CommandExecutor {
 							if (target.getName() != senderName) {
 								target = null;
 								prefix = "";
-								MessageUtils.message(sender,
-										"&cYou do not have permission to edit the prefix of other players. Usage: /prefix <prefix>");
+								MessageUtils.configStringMessage(sender, "prefix.permission-error");
 								return false;
 							}
 						} else {
@@ -61,8 +58,7 @@ public class Prefix implements CommandExecutor {
 						}
 					}
 				} else if (args.length > 2) {
-					MessageUtils.message(sender,
-							"&4&l(!)&c Too many arguments were provided! Correct format: /prefix [player] <prefix>");
+					MessageUtils.configStringMessage(sender, "prefix.argument-error");
 					return false;
 				}
 
@@ -73,19 +69,19 @@ public class Prefix implements CommandExecutor {
 					UUID targetId = target.getUniqueId();
 					String targetName = target.getName();
 
-					if (prefix != "reset") {
+					if (!prefix.equalsIgnoreCase("reset")) {
 						playersFile.set("Players." + targetId + ".prefix", prefix);
 						playersFileManager.saveData();
 					} else {
-						MessageUtils.message(sender, "&4&l(!)&c Please provide a valid prefix!");
+						MessageUtils.configStringMessage(sender, "prefix.invalid-prefix-error");
 						return false;
 					}
 
 					if (targetName != senderName) {
-						MessageUtils.message(target, "&6&l(!) &eYour prefix has been set to " + prefix + "&e.");
+						MessageUtils.configStringMessage(target, "prefix.prefix-set-target-message");
 					}
-					MessageUtils.message(sender,
-							"&6&l(!)&e " + target.getName() + "&e's prefix has been set to " + prefix + "&e.");
+					MessageUtils.configStringMessage(sender, "prefix.prefix-set-message", "<player>",
+							target.getDisplayName());
 				}
 			} else {
 				MessageUtils.noPermissionMessage(sender);
