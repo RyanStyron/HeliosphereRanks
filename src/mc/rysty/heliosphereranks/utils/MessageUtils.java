ad332a4719e108b935c35ba86a46eddaf0f21c1b
Ryan Styron <ryanstyron1220@gmail.com>
2020-03-25 21:54:13 -0400

    Add default config.yml messages and functions in MessageUtils.yml.

--- src/mc/rysty/heliosphereranks/utils/MessageUtils.java
+++ src/mc/rysty/heliosphereranks/utils/MessageUtils.java
@@ -2,14 +2,35 @@ package mc.rysty.heliosphereranks.utils;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import mc.rysty.heliosphereranks.HelioSphereRanks;
 
 public class MessageUtils {
 
-	public static String convertChatColors(String string) {
-		return ChatColor.translateAlternateColorCodes('&', string);
-	}
+    private static FileConfiguration config = HelioSphereRanks.getInstance().getConfig();
+
+    public static String convertChatColors(String string) {
+        return ChatColor.translateAlternateColorCodes('&', string);
+    }
 
     public static void message(CommandSender sender, String message) {
         sender.sendMessage(convertChatColors(message));
+    }
+
+    public static void configStringMessage(CommandSender sender, String configString) {
+        message(sender, config.getString(configString));
+    }
+
+    public static void noPermissionMessage(CommandSender sender) {
+        configStringMessage(sender, "no-perm-message");
+    }
+
+    public static void consoleErrorMessage(CommandSender sender) {
+        configStringMessage(sender, "console-error-message");
+    }
+
+    public static void invalidPlayerMessage(CommandSender sender) {
+        configStringMessage(sender, "player-offline-message");
     }
 }
